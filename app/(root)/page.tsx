e9b9f482874fd5f64f70f09406d1690b8cbd765c
Nishant Yadav <nynishuyadav85@gmail.com>
2024-10-28 17:11:02 +0530

    Done auth changes using Github , created NavBar component , Created Hero Component , Configured tailwing css and Shadcn

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your StartUp,<br /> Connect with Vc's</h1>
+        <p className="sub-heading !max-w-3xl">Submit !deas, Vote, View and get noticed </p>
+      </section>
+
+    </>
+  );
+}
